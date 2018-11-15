2fa524fdc15928d43503a2329ad96c6affbf92f3
rodrigo <rodrigo@eldespacho.cl>
2018-11-15 10:33:45 -0300

    First Commit
    Upload proyect

---
+++ index.php
@@ -0,0 +1,265 @@
+<!doctype html>
+<html lang="es">
+    
+    <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      
+
+    <title>#NecesitamosUnRespiro</title>
+        
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="apple-touch-icon" href="icon.png">
+    <!-- Place favicon.ico in the root directory -->
+        
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:200,300,400,400i,500,500i,600,700,900" rel="stylesheet">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha256-eSi1q2PG6J7g7ib17yAaWMcrr5GrtohYChqibrV7PBE=" crossorigin="anonymous" />
+        
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+        
+    <link rel="stylesheet" href="style.css">
+
+    
+  </head>
+  <body>
+      
+    <!-- ****************************************************************************-->
+<section id="header-bg">     
+<header>
+    <div class="container-fluid">   
+        <div class="row justify-content-md-center">
+                
+            <div class="col col-md"></div>
+                
+            <div class="col-md-auto">
+                <div id="logo"><img src="images/nur-logo.jpg" class="mx-auto d-block img-fluid" ></div>
+            </div>
+                
+            <div class="col col-md text-right">
+                <div id="header-icons" class="social-icons">
+                    <a href="" target="_blank"><i class="fab fa-twitter"></i></a>
+                    <a href="" target="_blank"><i class="fab fa-youtube"></i></a>
+                    <a href="" target="_blank"><i class="fab fa-facebook-f"></i></a>
+                    <a href="" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+                    <a href="" target="_blank"><i class="fab fa-instagram"></i></a>
+                </div>
+                    
+            </div>
+                 
+        </div>
+    </div>
+</header>
+<main>      
+      <!-- ****************************************************************************-->
+
+      
+
+    <div class="container">
+        <div class="row">
+            
+            <div class="col-md-12">
+                <img src="images/vid.jpg" class="img-fluid mx-auto d-block">
+            </div>
+            
+        </div>
+        
+        <div class="row justify-content-center">
+            <div id="sec1-txt" class="col-md-8 align-self-center text-center">
+                <p>#NecesitamosUnRespiro es un llamado a tomar conciencia sobre la poca atención que recibe el Cáncer de Pulmón en nuestro país.<br>Una enfermedad que sólo el último año le quitó la vida a <br><strong>más de 3.000 personas, siendo el cáncer que más muerte provoca</strong>.</p>
+            </div>
+        </div>
+        
+    </div>
+</section>
+
+      <!-- ****************************************************************************-->
+
+
+<section id="sec-posters">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8 text-center">
+                <h2>Dale aire a esta campaña <br>y compártela en tus Redes Sociales</h2>
+            </div>
+        </div>
+        
+        <div id="infos-row" class="row">
+            
+            <div class="col-md-4">
+                
+                <div class="info-s">
+                    <img src="images/sec2-img.jpg" class="img-fluid mx-auto d-block">
+                </div>
+                
+                <div class="row info-sharer">
+                    <div class="col-md-6 text-right"><span>COMPARTE</span></div>
+                    <div class="col-md-6 text-left">
+                    
+                        <div class="sharer-list">
+                            <div class="share-ico" data-sharer="twitter" data-title="Checkout Sharer.js!" data-hashtags="awesome, sharer.js" data-url="https://ellisonleao.github.io/sharer.js/"><i class="fab fa-twitter"></i></div>
+                        
+                            <div class="share-ico" data-sharer="facebook" data-hashtag="hashtag" data-url="https://ellisonleao.github.io/sharer.js/"><i class="fab fa-facebook"></i></div>
+                        
+                            <div class="share-ico" data-sharer="linkedin" data-url="https://ellisonleao.github.io/sharer.js/"><i class="fab fa-linkedin"></i></div> 
+                        </div>
+                    </div>  
+                </div>
+            </div>
+           <!-- **************COL4******************* --> 
+            
+            <div class="col-md-4">
+                
+                <div class="info-s">
+                    <img src="images/sec2-img.jpg" class="img-fluid mx-auto d-block">
+                </div>
+                
+                <div class="row info-sharer">
+                    <div class="col-md-6 text-right"><span>COMPARTE</span></div>
+                    <div class="col-md-6 text-left">
+                    
+                        <div class="sharer-list">
+                            <div class="share-ico" data-sharer="twitter" data-title="Checkout Sharer.js!" data-hashtags="awesome, sharer.js" data-url="https://ellisonleao.github.io/sharer.js/"><i class="fab fa-twitter"></i></div>
+                        
+                            <div class="share-ico" data-sharer="facebook" data-hashtag="hashtag" data-url="https://ellisonleao.github.io/sharer.js/"><i class="fab fa-facebook"></i></div>
+                        
+                            <div class="share-ico" data-sharer="linkedin" data-url="https://ellisonleao.github.io/sharer.js/"><i class="fab fa-linkedin"></i></div> 
+                        </div>
+                    </div>  
+                </div>
+            </div>
+           <!-- **************COL4******************* --> 
+            
+            <div class="col-md-4">
+                
+                <div class="info-s">
+                    <img src="images/sec2-img.jpg" class="img-fluid mx-auto d-block">
+                </div>
+                
+                <div class="row info-sharer">
+                    <div class="col-md-6 text-right"><span>COMPARTE</span></div>
+                    <div class="col-md-6 text-left">
+                    
+                        <div class="sharer-list">
+                            <div class="share-ico" data-sharer="twitter" data-title="Checkout Sharer.js!" data-hashtags="awesome, sharer.js" data-url="https://ellisonleao.github.io/sharer.js/"><i class="fab fa-twitter"></i></div>
+                        
+                            <div class="share-ico" data-sharer="facebook" data-hashtag="hashtag" data-url="https://ellisonleao.github.io/sharer.js/"><i class="fab fa-facebook"></i></div>
+                        
+                            <div class="share-ico" data-sharer="linkedin" data-url="https://ellisonleao.github.io/sharer.js/"><i class="fab fa-linkedin"></i></div> 
+                        </div>
+                    </div>  
+                </div>
+            </div>
+           <!-- **************COL4******************* --> 
+        </div>
+    </div>
+</section>
+<!-- ****************************************************************************-->
+
+<section id="sec-testimonios">
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8 text-center">
+                <h2><strong>Testimonios e informe médico</strong></h2>
+            </div>
+        </div>
+    </div>
+    
+    <div id="testimonios" class="container">
+        <div class="row">
+            
+            <div class="col-md-4 doc-block">
+                <img src="https://previews.123rf.com/images/ximagination/ximagination1503/ximagination150300483/37681251-retrato-de-la-persona-gorda-vistiendo-ropa-deportiva-y-sec%C3%A1ndose-el-sudor-con-una-toalla-aislado-en-bl.jpg" class="img-fluid mx-auto d-block">
+                <p>Dr Mauricio Burotto, Oncólogo Médico del Instituto Bradford habla sobre cáncer de pulmón y los nuevos tratamientos. Escucha la entrevista en el siguiente link.</p>
+            </div>
+            
+            <div class="col-md-4 doc-block">
+                <a href="https://www.roche.com/research_and_development/what_we_are_working_on/oncology/lung-cancer/lung-cancer-and-me.htm" target="_blank"><img src="https://previews.123rf.com/images/ximagination/ximagination1503/ximagination150300483/37681251-retrato-de-la-persona-gorda-vistiendo-ropa-deportiva-y-sec%C3%A1ndose-el-sudor-con-una-toalla-aislado-en-bl.jpg" class="img-fluid mx-auto d-block"></a>
+                <a href="https://www.roche.com/research_and_development/what_we_are_working_on/oncology/lung-cancer/lung-cancer-and-me.htm" target="_blank"><p>“Estoy muy feliz de ser un defensor en estos días porque muchas cosas buenas han comenzado a suceder en los últimos tres, cuatro, cinco años. El número de sobrevivientes está aumentando todo el tiempo, y es muy bueno poder decir eso”.</p></a>
+                <p><strong>Tommy Björk, miembro de la junta, Cáncer de Pulmón Europa</strong></p>
+            </div>
+            
+            <div class="col-md-4 doc-block">
+                <a href="https://www.roche.com/research_and_development/what_we_are_working_on/oncology/lung-cancer/lung-cancer-and-me.htm" target="_blank"><img src="https://previews.123rf.com/images/ximagination/ximagination1503/ximagination150300483/37681251-retrato-de-la-persona-gorda-vistiendo-ropa-deportiva-y-sec%C3%A1ndose-el-sudor-con-una-toalla-aislado-en-bl.jpg" class="img-fluid mx-auto d-block"></a>
+                <a href="https://www.roche.com/research_and_development/what_we_are_working_on/oncology/lung-cancer/lung-cancer-and-me.htm" target="_blank"><p>“Reconocemos que el viaje del cáncer de pulmón de cada paciente es diferente”.</p></a>
+                <p><strong>Dr. Alan Sandler, Global Head para Cáncer de Pulmón, Cabeza y Cuello.Roche</strong></p>
+            </div>
+            
+        </div>
+    </div>
+    
+    <div id="downloads" class="container">
+        <div class="row">
+            
+            <div class="col-md-6">
+                AQUI BOTON INFOGRAFIA
+            </div>
+            
+            <div class="col-md-6">
+                <div id="bot-report"><img src="images/bot-report.png" class="img-fluid mx-auto d-block"></div>
+            </div>
+            
+            
+        </div>
+    </div>
+    
+</section>      
+<!-- ****************************************************************************-->
+</main>
+<footer>
+    
+    <div class="container">   
+        <div class="row justify-content-md-center">
+                
+            <div class="col col-md">
+                <img src="images/logo-roche.png" class="img-fluid">
+            </div>
+                
+            <div class="col-md-auto">
+            </div>
+                
+            <div class="col col-md text-right">
+                <div id="footer-icons" class="social-icons">
+                    <a href="" target="_blank"><i class="fab fa-twitter"></i></a>
+                    <a href="" target="_blank"><i class="fab fa-youtube"></i></a>
+                    <a href="" target="_blank"><i class="fab fa-facebook-f"></i></a>
+                    <a href="" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+                    <a href="" target="_blank"><i class="fab fa-instagram"></i></a>
+                </div>
+                    
+            </div>
+                 
+        </div>
+    </div>
+      
+</footer>
+  
+<!-- ****************************************************************************-->
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+      
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+      
+      <script src="js/vendor/modernizr-3.6.0.min.js"></script>
+      <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
+      <script src="js/plugins.js"></script>
+      <script src="js/main.js"></script>
+      
+      <script src="js/vendor/modernizr-3.6.0.min.js"></script>
+        
+      <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+      
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/sharer.js/0.3.5/sharer.min.js"></script>
+      
+      
+  </body>
+</html>
